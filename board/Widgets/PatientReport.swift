f3253893437b101b5fdfff0f7a663085e8e4dc45
CarmonaD <Dacarma@2235>
2023-09-29 15:54:23 +0200

    add first widget

---
+++ board/Widgets/PatientReport.swift
@@ -0,0 +1,18 @@
+//
+//  PatientReport.swift
+//  vision-pro-board
+//
+//  Created by David Carmona on 29/9/23.
+//
+
+import SwiftUI
+
+struct PatientReport: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PatientReport()
+}
