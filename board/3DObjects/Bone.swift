fa48d0e68c831475705da8178603a5828a53e926
CarmonaD <Dacarma@2235>
2023-10-16 08:43:32 +0200

    add 3d bone

---
+++ board/3DObjects/Bone.swift
@@ -0,0 +1,18 @@
+//
+//  Bone.swift
+//  vision-pro-board
+//
+//  Created by David Carmona on 13/10/23.
+//
+
+import SwiftUI
+
+struct Bone: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Bone()
+}
---
+++ board/Widgets/Radiography.swift
@@ -0,0 +1,18 @@
+//
+//  Radiography.swift
+//  vision-pro-board
+//
+//  Created by David Carmona on 11/10/23.
+//
+
+import SwiftUI
+
+struct Radiography: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Radiography()
+}
